8e7aaa0e8b2820f31a95fdc25c25dd3528ed9718
Oussama Moutafatin <moutafatin3@gmail.com>
2022-04-23 23:54:50 +0000

    changes exports

---
+++ src/components/RatingComponent/Animation/index.tsx
@@ -0,0 +1,4 @@
+import { Fade } from "./Fade";
+import { StartAnimation } from "./StartAnimation";
+
+export { Fade, StartAnimation };
--- src/components/RatingComponent/index.tsx
+++ src/components/RatingComponent/index.tsx
@@ -1,12 +1,10 @@
 import RatingContent from "./RatingContent";
 import ThankYouState from "./ThankYouState";
-import { motion } from "framer-motion";
+import { useState } from "react";
+import { StartAnimation } from "./Animation";
+import { Fade } from "./Animation";
 
 const buttonsText = ["1", "2", "3", "4", "5"];
-
-import React, { useState } from "react";
-import { Fade } from "./Animation/Fade";
-import { StartAnimation } from "./Animation/StartAnimation";
 
 const RatingComponent = () => {
   const [submited, setSubmited] = useState(false);
@@ -15,19 +13,19 @@ const RatingComponent = () => {
     <div className="h-screen flex justify-center items-center bg-[#121417]">
       {/* Card */}
       <StartAnimation className="bg-[#202731] w-[27rem] h-[26rem] sm:h-[25rem] rounded-2xl mx-5">
-          {/* card content */}
-          {!submited && (
-            <RatingContent
-              buttonsText={buttonsText}
-              setRating={setRating}
-              setSubmited={setSubmited}
-            />
-          )}
-          {submited && (
-            <Fade>
-              <ThankYouState rating={rating} />
-            </Fade>
-          )}
+        {/* card content */}
+        {!submited && (
+          <RatingContent
+            buttonsText={buttonsText}
+            setRating={setRating}
+            setSubmited={setSubmited}
+          />
+        )}
+        {submited && (
+          <Fade>
+            <ThankYouState rating={rating} />
+          </Fade>
+        )}
       </StartAnimation>
     </div>
   );
