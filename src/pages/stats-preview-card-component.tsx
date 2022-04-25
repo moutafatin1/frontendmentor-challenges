90db0f39b36901d8aa5950740258e843ae065d9a
Oussama Moutafatin <moutafatin3@gmail.com>
2022-04-25 22:53:55 +0000

    little margin tweak

--- src/pages/stats-preview-card-component.tsx
+++ src/pages/stats-preview-card-component.tsx
@@ -18,7 +18,7 @@ const StatePreviewCard = () => {
         {/* Card */}
         <StartAnimation className="w-80 h-[40rem] bg-[#1b1938] lg:w-full max-w-5xl lg:h-96 rounded-xl lg:grid grid-cols-2">
           <figure className="bg-[url('/stats-preview-card/image-header-mobile.jpg')] bg-[#aa5cdb] bg-blend-multiply  bg-cover bg-no-repeat object-contain h-[35%] rounded-tl-xl rounded-tr-xl order-2 lg:rounded-br-xl lg:rounded-tl-none lg:h-96 "></figure>
-          <section className="text-center mx-10 my-8 lg:text-left lg:m-16 lg:space-y-7">
+          <section className="text-center mx-10 my-8 lg:text-left lg:my-16 lg:ml-16 lg:mr-32 lg:space-y-7">
             <h1 className="text-white text-xl font-bold font-deca lg:text-3xl ">
               Get <span className="text-[#aa5cdb]">insights</span> that help
               your business grow.
