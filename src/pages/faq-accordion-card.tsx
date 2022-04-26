c2a0006fa458a4dd368fbd24ce3124f9d290ae03
Oussama Moutafatin <moutafatin3@gmail.com>
2022-04-26 15:35:00 +0000

    add page title

--- src/pages/faq-accordion-card.tsx
+++ src/pages/faq-accordion-card.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import { Accordion } from "../components/Accordion/Accordion";
 import { StartAnimation } from "../components/Animation";
@@ -37,76 +38,90 @@ const faqContent = [
 
 const FaqAccordionCard = () => {
   return (
-    <main className="min-h-screen font-kumbh bg-gradient-to-b from-[#af67e9] to-[#6565e7] flex justify-center items-center">
-      <StartAnimation className="bg-white w-80 h-full rounded-3xl px-4 lg:pr-16 lg:pl-0 py-6 lg:w-full lg:mx-52 lg:flex justify-between  shadow-2xl">
-        {/* Mobile Image */}
-        <section className="relative lg:hidden">
-          <figure className="absolute  h-64 w-64 -translate-x-1/2 -translate-y-1/2 left-1/2  z-10">
+    <>
+      <Head>
+        <title>FAQ Accordion Card Component</title>
+        <meta
+          name="description"
+          content="FAQ Accordion Card Component Challenge from Frontend Mentor"
+        />
+        <link rel="icon" href="/3-column-preview-card/favicon-32x32.png" />
+      </Head>
+      <main className="min-h-screen font-kumbh bg-gradient-to-b from-[#af67e9] to-[#6565e7] flex justify-center items-center">
+        <StartAnimation className="bg-white w-80 h-full rounded-3xl px-4 lg:pr-16 lg:pl-0 py-6 lg:w-full lg:mx-52 lg:flex justify-between  shadow-2xl">
+          {/* Mobile Image */}
+          <section className="relative lg:hidden">
+            <figure className="absolute  h-64 w-64 -translate-x-1/2 -translate-y-1/2 left-1/2  z-10">
+              <Image
+                src="/faq-accordion-card/illustration-woman-online-mobile.svg"
+                alt="woman mobile"
+                layout="fill"
+                objectFit="contain"
+                className=""
+              />
+            </figure>
+            <figure className="absolute  h-64 w-64 -translate-x-1/2 -translate-y-1/4 top-3 left-1/2 ">
+              <Image
+                src="/faq-accordion-card/bg-pattern-mobile.svg"
+                alt="woman mobile"
+                layout="fill"
+                objectFit="contain"
+                className=""
+              />
+            </figure>
+          </section>
+          {/* Desktop Image */}
+
+          <section className="relative hidden lg:flex w-1/2 overflow-hidden z-20">
+            {/* woman */}
+            <figure className="absolute  h-96 w-96 -left-16    z-10">
+              <Image
+                src="/faq-accordion-card/illustration-woman-online-desktop.svg"
+                alt="woman desktop"
+                layout="fill"
+                objectFit="contain"
+                className=""
+              />
+            </figure>
+            {/* shadow */}
+            <figure className="absolute  h-[50rem] w-[50rem] -translate-x-1/2 -translate-y-1/3 -left-24  ">
+              <Image
+                src="/faq-accordion-card/bg-pattern-desktop.svg"
+                alt="pattern desktop"
+                layout="fill"
+                objectFit="contain"
+                className=""
+              />
+            </figure>
+          </section>
+          {/* Box */}
+
+          <figure className="absolute hidden lg:block h-52 w-52 translate-x-1/2 translate-y-1/2 z-20 -left-6 top-44">
             <Image
-              src="/faq-accordion-card/illustration-woman-online-mobile.svg"
+              src="/faq-accordion-card/illustration-box-desktop.svg"
               alt="woman mobile"
               layout="fill"
               objectFit="contain"
-              className=""
+              className="shadow-2xl "
             />
           </figure>
-          <figure className="absolute  h-64 w-64 -translate-x-1/2 -translate-y-1/4 top-3 left-1/2 ">
-            <Image
-              src="/faq-accordion-card/bg-pattern-mobile.svg"
-              alt="woman mobile"
-              layout="fill"
-              objectFit="contain"
-              className=""
-            />
-          </figure>
-        </section>
-        {/* Desktop Image */}
-
-        <section className="relative hidden lg:flex w-1/2 overflow-hidden z-20">
-          {/* woman */}
-          <figure className="absolute  h-96 w-96 -left-16    z-10">
-            <Image
-              src="/faq-accordion-card/illustration-woman-online-desktop.svg"
-              alt="woman desktop"
-              layout="fill"
-              objectFit="contain"
-              className=""
-            />
-          </figure>
-          {/* shadow */}
-          <figure className="absolute  h-[50rem] w-[50rem] -translate-x-1/2 -translate-y-1/3 -left-24  ">
-            <Image
-              src="/faq-accordion-card/bg-pattern-desktop.svg"
-              alt="pattern desktop"
-              layout="fill"
-              objectFit="contain"
-              className=""
-            />
-          </figure>
-        </section>
-        {/* Box */}
-
-        <figure className="absolute hidden lg:block h-52 w-52 translate-x-1/2 translate-y-1/2 z-20 -left-6 top-44">
-          <Image
-            src="/faq-accordion-card/illustration-box-desktop.svg"
-            alt="woman mobile"
-            layout="fill"
-            objectFit="contain"
-            className="shadow-2xl "
-          />
-        </figure>
-        <section className="mt-36 lg:w-1/2 lg:my-16   ">
-          <h1 className="text-center text-3xl font-bold lg:text-left lg:text-4xl ml-3">
-            FAQ
-          </h1>
-          {faqContent.map((faq) => {
-            return (
-              <Accordion key={faq.id} title={faq.title} content={faq.content} />
-            );
-          })}
-        </section>
-      </StartAnimation>
-    </main>
+          <section className="mt-36 lg:w-1/2 lg:my-16   ">
+            <h1 className="text-center text-3xl font-bold lg:text-left lg:text-4xl ml-3">
+              FAQ
+            </h1>
+            {faqContent.map((faq) => {
+              return (
+                <Accordion
+                  key={faq.id}
+                  title={faq.title}
+                  content={faq.content}
+                />
+              );
+            })}
+          </section>
+        </StartAnimation>
+      </main>
+    </>
   );
 };
 
