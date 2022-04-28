46c96375b6b6187b10d80589a9d7e8fe07c7f6cd
Oussama Moutafatin <moutafatin3@gmail.com>
2022-04-28 01:25:26 +0000

    add title

--- src/pages/manage-landing-page.tsx
+++ src/pages/manage-landing-page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import {
   CallToAction,
@@ -10,14 +11,24 @@ import {
 
 const ManageLandingPage = () => {
   return (
-    <div className="font-poppins bg-tablet-pattern bg-no-repeat bg-[length:500px]  bg-[right_-8%]  ">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Testimonials />
-      <CallToAction />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Manage Landing Page</title>
+        <meta
+          name="description"
+          content="Manage Landing Page Challenge from Frontend Mentor"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="font-poppins bg-tablet-pattern bg-no-repeat bg-[length:500px]  bg-[right_-8%]  ">
+        <Navbar />
+        <Hero />
+        <Features />
+        <Testimonials />
+        <CallToAction />
+        <Footer />
+      </div>
+    </>
   );
 };
 
